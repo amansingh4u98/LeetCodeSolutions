b99932f91269aca90f8512ad36d1996f3d581c12
Aman Singh <amansingh199835@gmail.com>
2024-05-21 13:45:58 +0000

    DLC 21/05/24

---
+++ 78.subSets.java
@@ -0,0 +1,24 @@
+//T.C : O(2^n)
+//S.C :  O(2^n*length of each subset) to store each subset
+//       The recursion stack space complexity is O(N) , the maximum depth of the recursion is N, where N is the length of the input array.
+public class Solution {
+    private List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        solve(nums, 0, temp);
+        return result;
+    }
+
+    private void solve(int[] nums, int idx, List<Integer> temp) {
+        if (idx >= nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        temp.add(nums[idx]);
+        solve(nums, idx + 1, temp);
+        temp.remove(temp.size() - 1);
+        solve(nums, idx + 1, temp);
+    }
+}
